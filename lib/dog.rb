648972c31f105a5ffecbfe36a6d35a559063adde
Emily Welsch <emwelsch@gmail.com>
2018-02-16 21:24:45 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,5 @@
 class Dog
-  # def initialize(name)
-  #   @name = name
-  # end
-  #
-  # def name
-  #   @name
-  # end
+  
   def name=(dogs_name)
       @name = dogs_name
     end
@@ -20,11 +14,18 @@ class Dog
 
   puts fido.name
 
-#   def initialize(breed)
-#     @breed = breed
-#   end
-#
-#   def breed
-#     @breed
-#   end
-# end
+  def breed=(dogs_breed)
+      @breed = dogs_breed
+    end
+
+    def breed
+      @breed
+    end
+  end
+
+  beagle = Dog.new
+  beagle.breed = "Beagle"
+
+  puts beagle.breed
+
+  
