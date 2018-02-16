108eb2d36f06beca0162452123e6bd9872ca0038
Emily Welsch <emwelsch@gmail.com>
2018-02-16 21:19:22 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,11 @@
 class Dog
-  def initialize(name)
-    @name = name
-  end
-
-  def name
-    @name
-  end
+  # def initialize(name)
+  #   @name = name
+  # end
+  #
+  # def name
+  #   @name
+  # end
   def name=(dogs_name)
       @this_dogs_name = dogs_name
     end
